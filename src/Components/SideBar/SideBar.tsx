4e3a5bc21a65510f7f49d41f2f6b8bb96bcafdbf
hyun-EG <codiee@naver.com>
2024-08-11 17:35:17 +0900

    Design: FE-47 최종 수정본 사이드바 디자인 반영

--- src/Components/SideBar/SideBar.tsx
+++ src/Components/SideBar/SideBar.tsx
@@ -1,62 +1,84 @@
-import logo from '../../assets/icon-logo.svg';
-import dashboard from '../../assets/icon-dashboard.svg';
-import same from '../../assets/icon-same.svg';
-import alert from '../../assets/icon-alert.svg';
-import mytask from '../../assets/icon-my-task.svg';
-import mypage from '../../assets/icon-my-page.svg';
-import admin from '../../assets/icon-admin.svg';
-import arrow from '../../assets/icon-arrow.svg';
+import React from 'react';
+import dashboard from '../../assets/icon-dashboard-w.svg';
+import same from '../../assets/icon-same-w.svg';
+import alert from '../../assets/icon-alert-w.svg';
+import mytask from '../../assets/icon-my-task-w.svg';
+import mypage from '../../assets/icon-my-page-w.svg';
+import admin from '../../assets/icon-admin-w.svg';
+import clickedDashboard from '../../assets/icon-dashboard.svg';
+import clickedSame from '../../assets/icon-same.svg';
+import clickedAlert from '../../assets/icon-alert.svg';
+import clickedMytask from '../../assets/icon-my-task.svg';
+import clickedMypage from '../../assets/icon-my-page.svg';
+import clickedAdmin from '../../assets/icon-admin.svg';
 
 const SideBar = () => {
   return (
     <div className="sidebar">
       <div className="sidebar__container">
         <div className="sidebar__container__logo-box">
-          <img src={logo} alt="로고사진임" />
+          <div className="sidebar__container__logo-box__logo">한국광고자율심의기구</div>
         </div>
-        <div className="sidebar__container__logo-border" />
+        <div className="sidebar__container__border" />
         <div className="sidebar__container__nav-box">
-          <div className="sidebar__container__nav-box__user">
-            <div className="sidebar__container__nav-box__user-nav">
-              <div className="sidebar__container__nav-box__user-nav-bar" />
-              <img className="sidebar__container__nav-box__user-nav-icon" src={dashboard} alt="대시보드아이콘임" />
-              <div className="sidebar__container__nav-box__user-nav-title">대시보드</div>
-            </div>
-            <div className="sidebar__container__nav-box__user-nav">
-              <div className="sidebar__container__nav-box__user-nav-bar" />
-              <img className="sidebar__container__nav-box__user-nav-icon" src={same} alt="동일광고아이콘임" />
-              <div className="sidebar__container__nav-box__user-nav-title">동일광고</div>
-            </div>
-            <div className="sidebar__container__nav-box__user-nav">
-              <div className="sidebar__container__nav-box__user-nav-bar" />
-              <img className="sidebar__container__nav-box__user-nav-icon" src={alert} alt="지적광고아이콘임" />
-              <div className="sidebar__container__nav-box__user-nav-title">지적광고</div>
-            </div>
-            <div className="sidebar__container__nav-box__user-nav">
-              <div className="sidebar__container__nav-box__user-nav-bar" />
-              <img className="sidebar__container__nav-box__user-nav-icon" src={mytask} alt="나의작업아이콘임" />
-              <div className="sidebar__container__nav-box__user-nav-title">나의 작업</div>
-            </div>
-            <div className="sidebar__container__nav-box__user-nav">
-              <div className="sidebar__container__nav-box__user-nav-bar" />
-              <img className="sidebar__container__nav-box__user-nav-icon" src={mypage} alt="마이페이지아이콘임" />
-              <div className="sidebar__container__nav-box__user-nav-title">마이페이지</div>
+          <div className="sidebar__container__nav-box__item">
+            <div className="sidebar__container__nav-box__item-bar" />
+            <div className="sidebar__container__nav-box__item-area">
+              <img className="sidebar__container__nav-box__item-area__icon" src={dashboard} alt="대시보드" />
+              <span className="text-content">대시보드</span>
             </div>
           </div>
-          <div className="sidebar__container__nav-box__manager">
-            <div className="sidebar__container__nav-box__manager-nav">
-              <div className="sidebar__container__nav-box__manager-nav-bar" />
-              <img className="sidebar__container__nav-box__manager-nav-icon" src={admin} alt="관리자전용아이콘임" />
-              <div className="sidebar__container__nav-box__manager-nav-title">관리자 전용</div>
-              <img className="sidebar__container__nav-box__manager-nav-arrow" src={arrow} alt="화살임" />
+          <div className="sidebar__container__nav-box__item">
+            <div className="sidebar__container__nav-box__item-bar" />
+            <div className="sidebar__container__nav-box__item-area">
+              <img className="sidebar__container__nav-box__item-area__icon" src={same} alt="동일광고" />
+              <span className="text-content">동일광고</span>
             </div>
-            <div className="sidebar__container__nav-box__list">
-              <div className="sidebar__container__nav-box__list-item">· 회원 승인 관리</div>
-              <div className="sidebar__container__nav-box__list-item">· 회원 정보 관리</div>
-              <div className="sidebar__container__nav-box__list-item">· 작업자 관리</div>
-              <div className="sidebar__container__nav-box__list-item">· 업무배정 관리</div>
-              <div className="sidebar__container__nav-box__list-item">· 작업물 등록</div>
+          </div>
+          <div className="sidebar__container__nav-box__item">
+            <div className="sidebar__container__nav-box__item-bar" />
+            <div className="sidebar__container__nav-box__item-area">
+              <img className="sidebar__container__nav-box__item-area__icon" src={alert} alt="지적광고" />
+              <span className="text-content">지적광고</span>
             </div>
+          </div>
+          <div className="sidebar__container__nav-box__item">
+            <div className="sidebar__container__nav-box__item-bar" />
+            <div className="sidebar__container__nav-box__item-area">
+              <img className="sidebar__container__nav-box__item-area__icon" src={mytask} alt="나의작업" />
+              <span className="text-content">나의 작업</span>
+            </div>
+          </div>
+          <div className="sidebar__container__nav-box__item">
+            <div className="sidebar__container__nav-box__item-bar" />
+            <div className="sidebar__container__nav-box__item-area">
+              <img className="sidebar__container__nav-box__item-area__icon" src={mypage} alt="마이페이지" />
+              <span className="text-content">마이페이지</span>
+            </div>
+          </div>
+        </div>
+        <div className="sidebar__container__admin-nav">
+          <div className="sidebar__container__admin-nav__item">
+            <div className="sidebar__container__admin-nav__item-bar" />
+            <div className="sidebar__container__admin-nav__item-area">
+              <img className="sidebar__container__admin-nav__item-area__icon" src={admin} alt="관리자메뉴" />
+              <span className="text-content">관리자메뉴</span>
+            </div>
+          </div>
+          <div className="sidebar__container__admin-nav__items">
+            <span className="sidebar__container__admin-nav__items__title">가입 요청 관리</span>
+          </div>
+          <div className="sidebar__container__admin-nav__items">
+            <span className="sidebar__container__admin-nav__items__title">회원 정보 관리</span>
+          </div>
+          <div className="sidebar__container__admin-nav__items">
+            <span className="sidebar__container__admin-nav__items__title">작업자 관리</span>
+          </div>
+          <div className="sidebar__container__admin-nav__items">
+            <span className="sidebar__container__admin-nav__items__title">업무배정 간리</span>
+          </div>
+          <div className="sidebar__container__admin-nav__items">
+            <span className="sidebar__container__admin-nav__items__title">작업물 등록</span>
           </div>
         </div>
       </div>
