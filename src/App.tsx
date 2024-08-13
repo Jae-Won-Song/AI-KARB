078300d4525befe87a05d789067582756c5ad3b7
Song Jae Won <jwsonghub@gmail.com>
2024-08-13 18:58:14 +0900

    Design: FE-35 스피너 디자인(모션 수정예정)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from 'react';
 import './App.css';
 import './styles/main.scss';
+import Spinner from './components/Spinner';
 
 const App = () => {
   const [count, setCount] = useState(0);
 
-  return <div>hello world</div>;
+  return <Spinner />;
 };
 
 export default App;
---
+++ src/components/Spinner.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Spinner = () => {
+  return (
+    <div className="spinner">
+      <div className="spinner__main" />
+      <h3>잠시만 기다려주세요</h3>
+    </div>
+  );
+};
+
+export default Spinner;
