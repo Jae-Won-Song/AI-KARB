07d1c3d79e3632d8fdec5f8dfa4327dffc090ab4
oxlzlo <yeojin58@naver.com>
2024-08-16 16:55:54 +0900

    Design: FE-63 정렬 필터 active 추가

--- src/components/SortFilter.tsx
+++ src/components/SortFilter.tsx
@@ -1,12 +1,20 @@
+import { useState } from 'react';
 import SortIcon from '../assets/Icon-sort.svg';
+import SortIconActive from '../assets/Icon-sort-a.svg';
 
 const SortFilter = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleActive = () => {
+    setIsActive(!isActive);
+  };
+
   return (
     <div className="sort">
-      <button className="sort__sortBtn">
+      <button className={`sort__sortBtn ${isActive ? 'sort__sortBtn--active' : ''}`} onClick={toggleActive}>
         <span className="sort__sortBtn__span">정렬</span>
         <div className="sort__sortBtn__icon">
-          <img src={SortIcon} alt="정렬 이미지" />
+          <img src={isActive ? SortIconActive : SortIcon} alt="정렬 이미지" />
         </div>
       </button>
     </div>
