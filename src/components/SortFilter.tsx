74249864da2886afd842628573564742fda17973
oxlzlo <yeojin58@naver.com>
2024-08-16 16:56:27 +0900

    Design: FE-63 정렬 필터 onBlur 추가

--- src/components/SortFilter.tsx
+++ src/components/SortFilter.tsx
@@ -9,9 +9,17 @@ const SortFilter = () => {
     setIsActive(!isActive);
   };
 
+  const handleBlur = () => {
+    setIsActive(false);
+  };
+
   return (
     <div className="sort">
-      <button className={`sort__sortBtn ${isActive ? 'sort__sortBtn--active' : ''}`} onClick={toggleActive}>
+      <button
+        className={`sort__sortBtn ${isActive ? 'sort__sortBtn--active' : ''}`}
+        onClick={toggleActive}
+        onBlur={handleBlur}
+      >
         <span className="sort__sortBtn__span">정렬</span>
         <div className="sort__sortBtn__icon">
           <img src={isActive ? SortIconActive : SortIcon} alt="정렬 이미지" />
