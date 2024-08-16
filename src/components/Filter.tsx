f960072e8781ed7cf03732cda55c2dbe0372b40d
oxlzlo <yeojin58@naver.com>
2024-08-16 16:03:19 +0900

    Feat: 필터 onBlur 추가

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -9,8 +9,12 @@ const Filter = () => {
     setIsActive(!isActive);
   };
 
+  const handleBlur = () => {
+    setIsActive(false);
+  };
+
   return (
-    <button className={`filterBtn ${isActive ? 'filterBtn--active' : ''}`} onClick={toggleActive}>
+    <button className={`filterBtn ${isActive ? 'filterBtn--active' : ''}`} onClick={toggleActive} onBlur={handleBlur}>
       <span className="filterBtn__span">필터</span>
       <div className="filterBtn__icon">
         <img src={isActive ? FilterIconActive : FilterIcon} alt="필터 이미지" />
