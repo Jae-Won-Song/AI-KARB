012f0575d8a86085d860d9062247969c7d3bc290
oxlzlo <yeojin58@naver.com>
2024-08-19 12:16:33 +0900

    Design: FE-52 버튼들 공통 컴포넌트 사용으로 변경

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,3 +1,5 @@
+import AuthButton from '../components/AuthButton';
+import Button from '../components/Button';
 import Input from '../components/Input';
 
 const SignUp = () => {
@@ -10,11 +12,15 @@ const SignUp = () => {
             <Input placeholder="이름" />
             <div className="signUp__wrapper__box_input_box">
               <Input placeholder="연락처 ('-'을 제외한 숫자만 입력)" size="small" />
-              <button>인증요청</button>
+              <Button type="button" state="disabled">
+                인증요청
+              </Button>
             </div>
             <div className="signUp__wrapper__box_input_box">
               <Input placeholder="아이디 (한글/특수문자 제외)" size="small" />
-              <button>중복확인</button>
+              <Button type="button" state="disabled">
+                중복확인
+              </Button>
             </div>
 
             <Input placeholder="비밀번호(영문자/숫자/특수문자 사용 가능, 8-16자)" type="password" />
@@ -23,7 +29,7 @@ const SignUp = () => {
             <Input placeholder="이메일" type="email" />
           </div>
           <div className="signUp__wrapper__box_button">
-            <button>회원가입</button>
+            <AuthButton disabled>회원가입</AuthButton>
           </div>
           <div className="signUp__wrapper__box_text">
             <span>이미 계정이 있으신가요? </span>
