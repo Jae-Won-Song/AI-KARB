e2721dbd01870bb9c6199eb5feb3ddc38ec6e2eb
hyun-EG <codiee@naver.com>
2024-08-22 20:19:38 +0900

    Docs: FE-62 경로 설정 및 문서 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import './styles/main.scss';
 import Layout from './components/Layout';
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>
-      <Layout>
-        <div>여따 나중에 라우터 넣으면됨</div>
-      </Layout>
-    </div>
+    <Routes>
+      {/* 로그인쪽 넣으세용 */}
+      <Route
+        path="/*"
+        element={
+          <Layout>
+            <Routes>
+              {/* <Routes>레이아웃 필요한거 넣으세용 <Route path="/mypage" element={<Mypage />} /></Routes> */}
+            </Routes>
+          </Layout>
+        }
+      />
+    </Routes>
   );
 };
 
