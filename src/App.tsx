6698e507e2b38aa528b4022a079ab229059af861
hyun-EG <codiee@naver.com>
2024-08-26 17:34:21 +0900

    Design: FE-77 탭 컴포넌트 디자인 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
 import './styles/main.scss';
 import Layout from './components/Layout';
 import { Routes, Route } from 'react-router-dom';
+import Tab from './components/Tab';
 
 const App = () => {
   return (
-    <Routes>
-      {/* 로그인쪽 넣으세용 */}
-      <Route
-        path="/*"
-        element={
-          <Layout>
-            <Routes>
-              {/* <Routes>레이아웃 필요한거 넣으세용 <Route path="/mypage" element={<Mypage />} /></Routes> */}
-            </Routes>
-          </Layout>
-        }
-      />
-    </Routes>
+    <Tab content="밥" styleName="active" />
+    // <Routes>
+    //   {/* 로그인쪽 넣으세용 */}
+    //   <Route
+    //     path="/*"
+    //     element={
+    //       <Layout>
+    //         <Routes>
+    //           {/* <Routes>레이아웃 필요한거 넣으세용 <Route path="/mypage" element={<Mypage />} /></Routes> */}
+    //         </Routes>
+    //       </Layout>
+    //     }
+    //   />
+    // </Routes>
   );
 };
 
