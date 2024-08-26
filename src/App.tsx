4ab2e1984a3784fb09edb7a15f4fd00cc4704bb6
hyun-EG <codiee@naver.com>
2024-08-26 19:17:04 +0900

    Docs: 앱 문서 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import './styles/main.scss';
 import Layout from './components/Layout';
 import { Routes, Route } from 'react-router-dom';
-import Tab from './components/Tab';
 
 const App = () => {
   return (
-    <Tab content="밥" styleName="active" />
-    // <Routes>
-    //   {/* 로그인쪽 넣으세용 */}
-    //   <Route
-    //     path="/*"
-    //     element={
-    //       <Layout>
-    //         <Routes>
-    //           {/* <Routes>레이아웃 필요한거 넣으세용 <Route path="/mypage" element={<Mypage />} /></Routes> */}
-    //         </Routes>
-    //       </Layout>
-    //     }
-    //   />
-    // </Routes>
+    <Routes>
+      {/* 로그인쪽 넣으세용 */}
+      <Route
+        path="/*"
+        element={
+          <Layout>
+            <Routes>
+              {/* <Routes>레이아웃 필요한거 넣으세용 <Route path="/mypage" element={<Mypage />} /></Routes> */}
+            </Routes>
+          </Layout>
+        }
+      />
+    </Routes>
   );
 };
 
