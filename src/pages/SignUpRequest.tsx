216f9673344f707e97e42ffb292735c54f2be840
Hyun-EG <codiee@naver.com>
2024-08-28 01:28:58 +0900

    Design: FE-72 가입요청페이지 디자인 1차 구현

--- src/pages/SignUpRequest.tsx
+++ src/pages/SignUpRequest.tsx
@@ -1,84 +1,153 @@
 import SearchBar from '../components/SearchBar';
 import Table from '../components/Table';
+import Tab from '../components/Tab';
+import check from '../assets/check-signup-request.svg';
 
 const SignUpRequest = () => {
   return (
     <div className="sign-up-request">
       <div className="sign-up-request__container">
-        <SearchBar />
+        <SearchBar>
+          <Tab styleName="hover" content="반려" />
+          <Tab styleName="active" content="승인" />
+        </SearchBar>
         <Table
           columns={[
             { name: '번호', width: '6.25vw' },
-            { name: '사원번호', width: '10.417vw' },
-            { name: '작업자', width: '10.417vw' },
-            { name: '전체작업', width: '12.5vw' },
-            { name: '미완료건', width: '12.5vw' },
-            { name: '완료건', width: '12.5vw' },
-            { name: '작업진척도', width: '17.708vw' },
+            {
+              name: '체크박스',
+              img: (
+                <img
+                  style={{ display: 'flex', justifyContent: 'center', width: '17px', height: '17px' }}
+                  src={check}
+                  alt="체크박스"
+                />
+              ),
+              width: '6.25vw',
+            },
+            { name: '이름', width: '8.333vw' },
+            { name: '사원번호', width: '8.75vw' },
+            { name: '연락처', width: '17.708vw' },
+            { name: '이메일', width: '21.875vw' },
+            { name: '가입요청일', width: '13.542vw' },
           ]}
           data={[
             {
               번호: 1,
-              사원번호: 'A13425',
-              작업자: '홍길동',
-              전체작업: '10건',
-              미완료건: '2건',
-              완료건: '8건',
-              작업진척도: '80%',
+              체크박스: <input className="checkBox" type="checkbox" />,
+              이름: '홍길동',
+              사원번호: '12345678',
+              연락처: '010-3985-9374',
+              이메일: 'yoon123@kakao.com',
+              가입요청일: '2024-12-12 13:35',
             },
             {
               번호: 1,
-              사원번호: 'A13425',
-              작업자: '홍길동',
-              전체작업: '10건',
-              미완료건: '2건',
-              완료건: '8건',
-              작업진척도: '80%',
+              체크박스: <input className="checkBox" type="checkbox" />,
+              이름: '홍길동',
+              사원번호: '12345678',
+              연락처: '010-3985-9374',
+              이메일: 'yoon123@kakao.com',
+              가입요청일: '2024-12-12 13:35',
             },
             {
               번호: 1,
-              사원번호: 'A13425',
-              작업자: '홍길동',
-              전체작업: '10건',
-              미완료건: '2건',
-              완료건: '8건',
-              작업진척도: '80%',
+              체크박스: <input className="checkBox" type="checkbox" />,
+              이름: '홍길동',
+              사원번호: '12345678',
+              연락처: '010-3985-9374',
+              이메일: 'yoon123@kakao.com',
+              가입요청일: '2024-12-12 13:35',
             },
             {
               번호: 1,
-              사원번호: 'A13425',
-              작업자: '홍길동',
-              전체작업: '10건',
-              미완료건: '2건',
-              완료건: '8건',
-              작업진척도: '80%',
+              체크박스: <input className="checkBox" type="checkbox" />,
+              이름: '홍길동',
+              사원번호: '12345678',
+              연락처: '010-3985-9374',
+              이메일: 'yoon123@kakao.com',
+              가입요청일: '2024-12-12 13:35',
             },
             {
               번호: 1,
-              사원번호: 'A13425',
-              작업자: '홍길동',
-              전체작업: '10건',
-              미완료건: '2건',
-              완료건: '8건',
-              작업진척도: '80%',
+              체크박스: <input className="checkBox" type="checkbox" />,
+              이름: '홍길동',
+              사원번호: '12345678',
+              연락처: '010-3985-9374',
+              이메일: 'yoon123@kakao.com',
+              가입요청일: '2024-12-12 13:35',
             },
             {
               번호: 1,
-              사원번호: 'A13425',
-              작업자: '홍길동',
-              전체작업: '10건',
-              미완료건: '2건',
-              완료건: '8건',
-              작업진척도: '80%',
+              체크박스: <input className="checkBox" type="checkbox" />,
+              이름: '홍길동',
+              사원번호: '12345678',
+              연락처: '010-3985-9374',
+              이메일: 'yoon123@kakao.com',
+              가입요청일: '2024-12-12 13:35',
             },
             {
               번호: 1,
-              사원번호: 'A13425',
-              작업자: '홍길동',
-              전체작업: '10건',
-              미완료건: '2건',
-              완료건: '8건',
-              작업진척도: '80%',
+              체크박스: <input className="checkBox" type="checkbox" />,
+              이름: '홍길동',
+              사원번호: '12345678',
+              연락처: '010-3985-9374',
+              이메일: 'yoon123@kakao.com',
+              가입요청일: '2024-12-12 13:35',
+            },
+            {
+              번호: 1,
+              체크박스: <input className="checkBox" type="checkbox" />,
+              이름: '홍길동',
+              사원번호: '12345678',
+              연락처: '010-3985-9374',
+              이메일: 'yoon123@kakao.com',
+              가입요청일: '2024-12-12 13:35',
+            },
+            {
+              번호: 1,
+              체크박스: <input className="checkBox" type="checkbox" />,
+              이름: '홍길동',
+              사원번호: '12345678',
+              연락처: '010-3985-9374',
+              이메일: 'yoon123@kakao.com',
+              가입요청일: '2024-12-12 13:35',
+            },
+            {
+              번호: 1,
+              체크박스: <input className="checkBox" type="checkbox" />,
+              이름: '홍길동',
+              사원번호: '12345678',
+              연락처: '010-3985-9374',
+              이메일: 'yoon123@kakao.com',
+              가입요청일: '2024-12-12 13:35',
+            },
+            {
+              번호: 1,
+              체크박스: <input className="checkBox" type="checkbox" />,
+              이름: '홍길동',
+              사원번호: '12345678',
+              연락처: '010-3985-9374',
+              이메일: 'yoon123@kakao.com',
+              가입요청일: '2024-12-12 13:35',
+            },
+            {
+              번호: 1,
+              체크박스: <input className="checkBox" type="checkbox" />,
+              이름: '홍길동',
+              사원번호: '12345678',
+              연락처: '010-3985-9374',
+              이메일: 'yoon123@kakao.com',
+              가입요청일: '2024-12-12 13:35',
+            },
+            {
+              번호: 1,
+              체크박스: <input className="checkBox" type="checkbox" />,
+              이름: '홍길동',
+              사원번호: '12345678',
+              연락처: '010-3985-9374',
+              이메일: 'yoon123@kakao.com',
+              가입요청일: '2024-12-12 13:35',
             },
           ]}
         />
