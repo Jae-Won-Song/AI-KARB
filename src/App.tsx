8d2d675024a83d425d0c91bc275bb181502c74dc
oxlzlo <yeojin58@naver.com>
2024-08-30 17:35:45 +0900

    Chore: FE-41 App.tsx 로그인 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './styles/main.scss';
 import Layout from './components/Layout';
 import { Routes, Route } from 'react-router-dom';
+import SignIn from './pages/Auth/SignIn';
 
 const App = () => {
   return (
     <Routes>
       {/* 로그인쪽 넣으세용 */}
+      <Route path="/signin" element={<SignIn />} />
       <Route
         path="/*"
         element={
