a7f7a68c7ae32abce11e82fb9a538fe6013539c9
oxlzlo <yeojin58@naver.com>
2024-09-02 18:11:59 +0900

    Chore: FE-53 App.tsx 유저정보 찾기 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './styles/main.scss';
 import Layout from './components/Layout';
 import { Routes, Route } from 'react-router-dom';
+import FindUser from './pages/Auth/FindUser';
 
 const App = () => {
   return (
     <Routes>
       {/* 로그인쪽 넣으세용 */}
+      <Route path="/find-user" element={<FindUser />} />
       <Route
         path="/*"
         element={
