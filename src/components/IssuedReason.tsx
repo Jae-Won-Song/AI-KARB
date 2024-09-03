8bf9940f71d639b74cbcd3d7f2421416e33461b9
oxlzlo <yeojin58@naver.com>
2024-09-04 02:15:53 +0900

    Feat: FE-84 props 추가 및 close 상태일 때 aricleContent 줄여서 표현

--- src/components/IssuedReason.tsx
+++ src/components/IssuedReason.tsx
@@ -4,20 +4,34 @@ import arrowUp from '../assets/arrow-up.svg';
 import iconTrash from '../assets/icon-trash.svg';
 import iconPencil from '../assets/icon-pencil.svg';
 
-const IssuedReason = () => {
+type IssuedReasonProps = {
+  contentNumber: number;
+  articleNumber: number;
+  articleTitle: string;
+  articleContent: string;
+};
+
+const IssuedReason = ({ contentNumber, articleNumber, articleTitle, articleContent }: IssuedReasonProps) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleToggle = () => {
     setIsOpen(!isOpen);
   };
 
+  const getShortArticleContent = (content: string) => {
+    if (content.length > 55) {
+      return `${content.substring(0, 55)}...`;
+    }
+    return content;
+  };
+
   return (
     <div className={`IssuedReason ${isOpen ? 'IssuedReason--open' : 'IssuedReason--close'}`}>
       <div className="IssuedReason__toggleBar">
         <div className="IssuedReason__toggleBar_title">
-          <div className="IssuedReason__toggleBar_title_number">#1</div>
-          <div className="IssuedReason__toggleBar_title_articleNum">제 11조</div>
-          <div className="IssuedReason__toggleBar_title_articleTitle">주장의 무입증</div>
+          <div className="IssuedReason__toggleBar_title_number">{`#${contentNumber}`}</div>
+          <div className="IssuedReason__toggleBar_title_articleNum">{`제 ${articleNumber}조`}</div>
+          <div className="IssuedReason__toggleBar_title_articleTitle">{articleTitle}</div>
         </div>
         <div className="IssuedReason__toggleBar_icon" onClick={handleToggle}>
           <img src={isOpen ? arrowUp : arrowDown} alt="위화살표" />
@@ -25,7 +39,7 @@ const IssuedReason = () => {
       </div>
       {isOpen && (
         <>
-          <div className="IssuedReason__content--open">일품진로 어쩌구 저쩌구 이러쿵 저러쿵</div>
+          <div className="IssuedReason__content--open">{articleContent}</div>
           <div className="IssuedReason__reviewComment">
             <div className="IssuedReason__reviewComment_title">검토 의견</div>
             <div className="IssuedReason__reviewComment_comment">없음</div>
@@ -37,7 +51,7 @@ const IssuedReason = () => {
           </div>
         </>
       )}
-      {!isOpen && <div className="IssuedReason__content--close">일품진로 어쩌구 저쩌구...</div>}
+      {!isOpen && <div className="IssuedReason__content--close">{getShortArticleContent(articleContent)}</div>}
     </div>
   );
 };
