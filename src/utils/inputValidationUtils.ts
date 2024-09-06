c422a91a0be0c25496a5485ad024d71ce710b915
oxlzlo <yeojin58@naver.com>
2024-09-06 18:21:13 +0900

    Feat: FE-89 input 유효성 검사에 이름 추가

--- src/utils/inputValidationUtils.ts
+++ src/utils/inputValidationUtils.ts
@@ -1,6 +1,9 @@
-export const validateEmail = (email: string): boolean => {
-  return false;
-}; // export default 안 쓰려고 임시로 잠깐 넣음
+export const validateName = (name: string): boolean => {
+  if (/^[가-힣]{2,4}$/.test(name)) {
+    return false;
+  }
+  return true;
+};
 
 export const validatePhoneNumber = (phoneNumber: string): boolean => {
   if (/^\d{11}$/.test(phoneNumber)) {
