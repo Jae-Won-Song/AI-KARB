fefa4cde164fa49b236b534ccfd4416036b9269f
oxlzlo <yeojin58@naver.com>
2024-09-08 17:19:29 +0900

    Refactor: FE-89 if문의 조건을 부정에서 긍정으로 바꿈으로서 가독성 향상

--- src/utils/inputValidationUtils.ts
+++ src/utils/inputValidationUtils.ts
@@ -1,13 +1,13 @@
 export const validateName = (name: string): boolean => {
   if (/^[가-힣]{2,4}$/.test(name)) {
-    return true;
+    return false;
   }
-  return false;
+  return true;
 };
 
 export const validatePhoneNumber = (phoneNumber: string): boolean => {
   if (/^\d{11}$/.test(phoneNumber)) {
-    return true;
+    return false;
   }
-  return false;
+  return true;
 };
