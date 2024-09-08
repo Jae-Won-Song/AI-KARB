a11f23a784a3eeab4285024b87891edc6ad87cad
oxlzlo <yeojin58@naver.com>
2024-09-08 17:35:46 +0900

    Comment: 유효성 검사 utils 파일에 주석 추가

--- src/utils/inputValidationUtils.ts
+++ src/utils/inputValidationUtils.ts
@@ -1,3 +1,5 @@
+// 유효성 검사
+// 이름
 export const validateName = (name: string): boolean => {
   if (/^[가-힣]{2,4}$/.test(name)) {
     return false;
@@ -5,6 +7,7 @@ export const validateName = (name: string): boolean => {
   return true;
 };
 
+// 연락처
 export const validatePhoneNumber = (phoneNumber: string): boolean => {
   if (/^\d{11}$/.test(phoneNumber)) {
     return false;
