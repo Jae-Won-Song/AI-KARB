faefacb33d469a4f5c38d1a9767a505a44d092d6
oxlzlo <yeojin58@naver.com>
2024-09-09 11:41:55 +0900

    Feat: FE-89 핸드폰으로 인증번호 요청 api 추가

--- src/api/auth/authApi.ts
+++ src/api/auth/authApi.ts
@@ -1,6 +1,14 @@
 import instance from '../apiConfig';
 
 /*
+ * 인증요청
+ * @param payload {type, phoneNumber}
+ */
+export const fetchSendCertNoDuringSignUp = (payload: { type: string; phoneNumber: string }) => {
+  return instance.post('/api/v1/auth/cert-no', payload);
+};
+
+/*
  * 인증번호 확인
  * @param payload {type, phoneNumber, certNo}
  */
