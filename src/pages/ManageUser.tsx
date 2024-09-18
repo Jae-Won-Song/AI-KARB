a383efdf0e97d96da375410e2173a834c416fbce
Hyun-EG <codiee@naver.com>
2024-09-18 17:25:55 +0900

    Feat: FE-108 회원목록 API 연결 및 렌더링

--- src/pages/ManageUser.tsx
+++ src/pages/ManageUser.tsx
@@ -1,8 +1,60 @@
+import { useEffect, useState } from 'react';
+import instance from '../api/apiConfig';
 import SearchBar from '../components/Common/SearchBar';
 import Table from '../components/Common/Table';
 import trash from '../assets/Icon-delete.svg';
+import Modal from '../components/Common/Modal';
+
+interface UserData {
+  cursorId: number;
+  empNo: string;
+  name: string;
+  authority: string;
+  userId: string;
+  phoneNum: string;
+  email: string;
+  signUpDate: string;
+  finalLoginDateTime: string;
+}
+
+interface ApiResponse {
+  data: {
+    contents: UserData[];
+  };
+}
 
 const ManageUser = () => {
+  const [userData, setUserData] = useState<UserData[]>([]);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await instance.get<ApiResponse>('/api/v1/admin/manage-user');
+        const { contents } = response.data?.data || {};
+        if (contents && Array.isArray(contents)) {
+          setUserData(contents);
+        }
+      } catch (error) {
+        console.error('에러', error);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
+  const formattedTableData = userData.map((user, index) => ({
+    번호: index + 1,
+    사원번호: user.empNo,
+    이름: user.name,
+    권한: user.authority,
+    아이디: user.userId,
+    연락처: user.phoneNum,
+    이메일: user.email,
+    회원가입일: user.signUpDate,
+    최종로그인일: user.finalLoginDateTime,
+    관리: <img style={{ paddingLeft: '5px' }} src={trash} alt="회원삭제" />,
+  }));
+
   return (
     <div className="manageUser">
       <div className="manageUser__container">
@@ -20,92 +72,7 @@ const ManageUser = () => {
             { name: '최종로그인일', width: '10.417vw' },
             { name: '관리', width: '6.25vw' },
           ]}
-          data={[
-            {
-              번호: 1,
-              사원번호: 'A12345',
-              이름: '김지후',
-              권한: '작업자',
-              아이디: 'dsdfse1234',
-              연락처: '010-2939-0101',
-              이메일: 'jung456@gmail.com',
-              회원가입일: '2024-12-12 13:35',
-              최종로그인일: '2024-12-12 13:35',
-              관리: <img style={{ paddingLeft: '5px' }} src={trash} alt="회원삭제" />,
-            },
-            {
-              번호: 1,
-              사원번호: 'A12345',
-              이름: '김지후',
-              권한: '작업자',
-              아이디: 'dsdfse1234',
-              연락처: '010-2939-0101',
-              이메일: 'jung456@gmail.com',
-              회원가입일: '2024-12-12 13:35',
-              최종로그인일: '2024-12-12 13:35',
-              관리: <img style={{ paddingLeft: '5px' }} src={trash} alt="회원삭제" />,
-            },
-            {
-              번호: 1,
-              사원번호: 'A12345',
-              이름: '김지후',
-              권한: '작업자',
-              아이디: 'dsdfse1234',
-              연락처: '010-2939-0101',
-              이메일: 'jung456@gmail.com',
-              회원가입일: '2024-12-12 13:35',
-              최종로그인일: '2024-12-12 13:35',
-              관리: <img style={{ paddingLeft: '5px' }} src={trash} alt="회원삭제" />,
-            },
-            {
-              번호: 1,
-              사원번호: 'A12345',
-              이름: '김지후',
-              권한: '작업자',
-              아이디: 'dsdfse1234',
-              연락처: '010-2939-0101',
-              이메일: 'jung456@gmail.com',
-              회원가입일: '2024-12-12 13:35',
-              최종로그인일: '2024-12-12 13:35',
-              관리: <img style={{ paddingLeft: '5px' }} src={trash} alt="회원삭제" />,
-            },
-            {
-              번호: 1,
-              사원번호: 'A12345',
-              이름: '김지후',
-              권한: '작업자',
-              아이디: 'dsdfse1234',
-              연락처: '010-2939-0101',
-              이메일: 'jung456@gmail.com',
-              회원가입일: '2024-12-12 13:35',
-              최종로그인일: '2024-12-12 13:35',
-              관리: <img style={{ paddingLeft: '5px' }} src={trash} alt="회원삭제" />,
-            },
-            {
-              번호: 1,
-              사원번호: 'A12345',
-              이름: '김지후',
-              권한: '작업자',
-              아이디: 'dsdfse1234',
-              연락처: '010-2939-0101',
-              이메일: 'jung456@gmail.com',
-              회원가입일: '2024-12-12 13:35',
-              최종로그인일: '2024-12-12 13:35',
-              관리: <img style={{ paddingLeft: '5px' }} src={trash} alt="회원삭제" />,
-            },
-            {
-              번호: 1,
-              사원번호: 'A12345',
-              이름: '김지후',
-              권한: '작업자',
-              아이디: 'dsdfse1234',
-              연락처: '010-2939-0101',
-              이메일: 'jung456@gmail.com',
-              회원가입일: '2024-12-12 13:35',
-              최종로그인일: '2024-12-12 13:35',
-              관리: <img style={{ paddingLeft: '5px' }} src={trash} alt="회원삭제" />,
-            },
-          ]}
+          data={formattedTableData}
         />
       </div>
     </div>
