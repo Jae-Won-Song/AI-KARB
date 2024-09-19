20df44faf2e7b1c562f21b787b85f62ac5be3318
oxlzlo <yeojin58@naver.com>
2024-09-20 01:36:28 +0900

    Feat: FE-111 api 요청 응답으로 온 지적광고 리스트 조회 렌더링

--- src/pages/IssueAd/IssueAd.tsx
+++ src/pages/IssueAd/IssueAd.tsx
@@ -6,10 +6,53 @@ import Filter from '../../components/Common/Filter';
 import Calendar from '../../components/Common/Calendar';
 import ReviewTag from '../../components/Common/ReviewTag';
 import { useNavigate } from 'react-router-dom';
-import { fetchLoadIssueAdDetail } from '../../api/issueAd/issueAdApi';
+import { fetchLoadIssueAdDetail, fetchLoadIssueAdList } from '../../api/issueAd/issueAdApi';
+import { useEffect, useState } from 'react';
+
+type Advertisement = {
+  adId: string;
+  media: string[];
+  category: string[];
+  product: string;
+  advertiser: string;
+  state: boolean;
+  issue: boolean;
+};
+
+type IssueAdList = {
+  totalElements: number;
+  advertisementList: Advertisement[];
+};
 
 const IssueAd = () => {
+  const [issueAdData, setIssueAdData] = useState<IssueAdList | null>(null);
+
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const payload = {
+      cursorInfo: {
+        cursorState: true,
+        cursorId: 'A00001',
+      },
+      keyword: null,
+      period: '2024-09-1',
+      state: null,
+      issue: null,
+      media: [],
+      category: [],
+    };
+
+    fetchLoadIssueAdList(payload)
+      .then((response) => {
+        if (response.data.code === 3405) {
+          setIssueAdData(response.data.data);
+        }
+      })
+      .catch((error) => {
+        console.error('지적광로 리스트 조회 요청 실패', error);
+      });
+  }, []);
 
   const handleRowClick = (advertisementId: string) => {
     fetchLoadIssueAdDetail({ advertisementId })
@@ -45,41 +88,35 @@ const IssueAd = () => {
           { name: '진행상황', width: '8.537vw' },
           { name: '지적비지적', width: '9.756vw' },
         ]}
-        data={[
-          {
-            번호: 1,
-            고유번호: '202409A00001',
-            매체명: '경상남도민일보',
-            업종구분: '식품',
-            상품명: '상품명상품명상품명',
-            광고주: '한천KPS',
-            담당자: '동일',
-            진행상황: <ReviewTag size="large" containerBg="#FFEDDA" circleBg="orange" content="검수전" />,
-            지적비지적: <ReviewTag size="large" containerBg="#FDDFE6" circleBg="red" content="지적" />,
-          },
-          {
-            번호: 1,
-            고유번호: '202409A00001',
-            매체명: '경상남도민일보',
-            업종구분: '식품',
-            상품명: '상품명상품명상품명',
-            광고주: '한천KPS',
-            담당자: '동일',
-            진행상황: <ReviewTag size="large" containerBg="#FFEDDA" circleBg="orange" content="검수전" />,
-            지적비지적: <ReviewTag size="large" containerBg="#FDDFE6" circleBg="red" content="지적" />,
-          },
-          {
-            번호: 1,
-            고유번호: '202409A00001',
-            매체명: '경상남도민일보',
-            업종구분: '식품',
-            상품명: '상품명상품명상품명',
-            광고주: '한천KPS',
-            담당자: '동일',
-            진행상황: <ReviewTag size="large" containerBg="#FFEDDA" circleBg="orange" content="검수전" />,
-            지적비지적: <ReviewTag size="large" containerBg="#FDDFE6" circleBg="red" content="지적" />,
-          },
-        ]}
+        data={
+          issueAdData?.advertisementList && issueAdData.advertisementList.length > 0
+            ? issueAdData.advertisementList.map((ad, index) => ({
+                번호: index + 1,
+                고유번호: ad.adId,
+                매체명: Array.isArray(ad.media) ? ad.media.join(', ') : ad.media,
+                업종구분: Array.isArray(ad.category) ? ad.category.join(', ') : ad.category,
+                상품명: ad.product,
+                광고주: ad.advertiser,
+                담당자: '담당자',
+                진행상황: (
+                  <ReviewTag
+                    size="large"
+                    containerBg={ad.state ? '#FFEDDA' : '#DEEEED'}
+                    circleBg={ad.state ? '#FFB566' : '#64ACA7'}
+                    content={ad.state ? '검수전' : '검수완료'}
+                  />
+                ),
+                지적비지적: (
+                  <ReviewTag
+                    size="large"
+                    containerBg={ad.issue ? '#FDDFE6' : '#D5EFF8'}
+                    circleBg={ad.issue ? '#EB003B' : '#4B98B2'}
+                    content={ad.issue ? '지적' : '비지적'}
+                  />
+                ),
+              }))
+            : []
+        }
         onRowClick={(row) => handleRowClick(row.고유번호)}
       />
     </main>
