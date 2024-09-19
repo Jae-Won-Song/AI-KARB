e1405f2d2b5e62299caca516e882636e9d5a6832
oxlzlo <yeojin58@naver.com>
2024-09-19 15:41:36 +0900

    Env: FE-60 배포 최신화

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // 루트 경로로 설정
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 });
