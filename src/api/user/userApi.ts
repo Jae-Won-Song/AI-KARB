7b790f8aaa7d001e663e4616b4f788da5b9b9b7b
Hyun-EG <codiee@naver.com>
2024-09-21 01:37:40 +0900

    Chore: 블록 구문 실수 수정

--- src/api/user/userApi.ts
+++ src/api/user/userApi.ts
@@ -1,6 +1,5 @@
 import instance from '../apiConfig';
 
-// eslint-disable-next-line import/prefer-default-export
 export const fetchMyTaskData = (cursorId: string | null, taskDataLength: number) => {
   const requestData = {
     cursorInfo: {
@@ -10,9 +9,9 @@ export const fetchMyTaskData = (cursorId: string | null, taskDataLength: number)
   };
 
   return instance.post('/api/v1/user/my-task', requestData);
-// 사용자 정보 수정
+};
 
-// eslint-disable-next-line import/prefer-default-export
+// 사용자 정보 수정
 export const updateUserInfo = (payload: { phoneNumber?: string; email?: string; certNoCheckToken?: string }) => {
   return instance.put('/api/v1/user/info', payload);
 };
