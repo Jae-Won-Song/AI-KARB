94e7346d1bc1835b756fcb25826a7a6ad4f0eca0
Hyun-EG <codiee@naver.com>
2024-09-21 00:44:57 +0900

    Chore: API 분리

--- src/api/admin/adminApi.ts
+++ src/api/admin/adminApi.ts
@@ -10,6 +10,28 @@ export const deleteUser = (userId: number) => {
   return instance.delete(`/api/v1/admin/manage-user/${userId}`);
 };
 
+// 가입 요청 목록 가져오기
+
+export const fetchSignUpRequests = () => {
+  return instance.get('/api/v1/admin/approve-user');
+};
+
+// 회원 승인 요청
+
+export const approveUsers = (selectedEmpNos: string[]) => {
+  return instance.post('/api/v1/admin/approve-user', {
+    userList: selectedEmpNos.map((empNo) => ({ empNo })),
+  });
+};
+
+// 회원 반려 요청
+
+export const rejectUsers = (selectedEmpNos: string[]) => {
+  return instance.post('/api/v1/admin/reject-user', {
+    userList: selectedEmpNos.map((empNo) => ({ empNo })),
+  });
+};
+
 export const fetchmytaskData = () => {
   return instance.post('/api/v1/user/my-task', {
     // keyword: 'keyword',
