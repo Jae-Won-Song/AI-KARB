cfa43ffbce6d04977ac766a5f6358b3f915598ba
oxlzlo <yeojin58@naver.com>
2024-09-23 00:09:31 +0900

    Feat: FE-125 동일광고 리스트 조회 api 연결

--- src/pages/SameAd/SameAd.tsx
+++ src/pages/SameAd/SameAd.tsx
@@ -4,8 +4,57 @@ import Table from '../../components/Common/Table';
 import TagFilter from '../../components/Common/TagFilter';
 import Filter from '../../components/Common/Filter';
 import Calendar from '../../components/Common/Calendar';
+import ReviewTag from '../../components/Common/ReviewTag';
+import { useEffect, useState } from 'react';
+import { fetchLoadSameAdList } from '../../api/sameAd/sameAdApi';
+import { useNavigate } from 'react-router-dom';
+
+type Advertisement = {
+  adId: string;
+  media: string[];
+  category: string[];
+  product: string;
+  advertiser: string;
+  same: boolean;
+};
+
+type SameAdList = {
+  totalElements: number;
+  sameAdvertisementList: Advertisement[];
+};
 
 const SameAd = () => {
+  const [sameAdData, setSameAdData] = useState<SameAdList | null>(null);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const payload = {
+      cursorId: '202409A00001',
+      keyword: null,
+      period: '2024-09-1',
+      same: true,
+      media: [],
+      category: [],
+    };
+
+    fetchLoadSameAdList(payload)
+      .then((response) => {
+        console.log(response);
+        if (response.data.code === 3700) {
+          console.log('동일광고 리스트', response);
+          setSameAdData(response.data.data);
+        }
+      })
+      .catch((error) => {
+        console.error('동일광고 리스트 조회 실패', error);
+      });
+  }, []);
+
+  const handleRowClick = (advertisementId: string) => {
+    navigate('/same-ad/result');
+  };
+
   return (
     <main className="SameAd">
       <SearchBar totalCount={7}>
@@ -16,79 +65,35 @@ const SameAd = () => {
       </SearchBar>
       <Table
         columns={[
-          { name: '번호', width: '88px', rowHeight: '5.926vh', columnHeight: '5.556vh' },
-          { name: '고유번호', width: '160px' },
-          { name: '매체명', width: '240px' },
-          { name: '업종구분', width: '200px' },
-          { name: '상품명', width: '420px' },
-          { name: '광고주', width: '240px' },
-          { name: '검수결과', width: '200px' },
+          { name: '번호', width: '6.250vw', rowHeight: '5.926vh', columnHeight: '5.556vh' },
+          { name: '고유번호', width: '8.333vw' },
+          { name: '매체명', width: '12.5vw' },
+          { name: '업종구분', width: '10.417vw' },
+          { name: '상품명', width: '21.875vw' },
+          { name: '광고주', width: '12.5vw' },
+          { name: '검수결과', width: '10.417vw' },
         ]}
-        data={[
-          {
-            번호: 1,
-            고유번호: 'A13425',
-            매체명: '경상남도민일보',
-            업종구분: '식품',
-            상품명: '상품명상품명상품명',
-            광고주: '한천KPS',
-            검수결과: '동일',
-          },
-          {
-            번호: 1,
-            고유번호: 'A13425',
-            매체명: '경상남도민일보',
-            업종구분: '식품',
-            상품명: '상품명상품명상품명',
-            광고주: '한천KPS',
-            검수결과: '동일',
-          },
-          {
-            번호: 1,
-            고유번호: 'A13425',
-            매체명: '경상남도민일보',
-            업종구분: '식품',
-            상품명: '상품명상품명상품명',
-            광고주: '한천KPS',
-            검수결과: '동일',
-          },
-          {
-            번호: 1,
-            고유번호: 'A13425',
-            매체명: '경상남도민일보',
-            업종구분: '식품',
-            상품명: '상품명상품명상품명',
-            광고주: '한천KPS',
-            검수결과: '동일',
-          },
-          {
-            번호: 1,
-            고유번호: 'A13425',
-            매체명: '경상남도민일보',
-            업종구분: '식품',
-            상품명: '상품명상품명상품명',
-            광고주: '한천KPS',
-            검수결과: '동일',
-          },
-          {
-            번호: 1,
-            고유번호: 'A13425',
-            매체명: '경상남도민일보',
-            업종구분: '식품',
-            상품명: '상품명상품명상품명',
-            광고주: '한천KPS',
-            검수결과: '동일',
-          },
-          {
-            번호: 1,
-            고유번호: 'A13425',
-            매체명: '경상남도민일보',
-            업종구분: '식품',
-            상품명: '상품명상품명상품명',
-            광고주: '한천KPS',
-            검수결과: '동일',
-          },
-        ]}
+        data={
+          sameAdData?.sameAdvertisementList && sameAdData.sameAdvertisementList.length > 0
+            ? sameAdData.sameAdvertisementList.map((ad, index) => ({
+                번호: index + 1,
+                고유번호: ad.adId,
+                매체명: Array.isArray(ad.media) ? ad.media.join(', ') : ad.media,
+                업종구분: Array.isArray(ad.category) ? ad.category.join(', ') : ad.category,
+                상품명: ad.product,
+                광고주: ad.advertiser,
+                검수결과: (
+                  <ReviewTag
+                    size="large"
+                    containerBg={ad.same ? '#D9DFF9' : '#E4E4E4'}
+                    circleBg={ad.same ? '#5A6CBD' : '#8E8E8E'}
+                    content={ad.same ? '동일' : '비동일'}
+                  />
+                ),
+              }))
+            : []
+        }
+        onRowClick={(row) => handleRowClick(row.고유번호)}
       />
     </main>
   );
