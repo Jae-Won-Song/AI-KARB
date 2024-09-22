a3952de933e4b6b7319607aa38035f203b6fb718
oxlzlo <yeojin58@naver.com>
2024-09-23 03:40:21 +0900

    Feat: FE-127 유사율 상세화면 조회 api 추가

--- src/api/sameAd/sameAdApi.ts
+++ src/api/sameAd/sameAdApi.ts
@@ -15,3 +15,17 @@ export const fetchLoadSameAdList = (payload: object) => {
 export const fetchLoadSameAdResult = ({ inspectionAdvertisementId }: { inspectionAdvertisementId: string }) => {
   return instance.get(`/api/v1/same-ad/result/${inspectionAdvertisementId}`);
 };
+
+/*
+ * 동일광고 유사율 상세화면 조회
+ * @params {advertisementId}
+ */
+export const fetchLoadSameAdResultDetail = ({
+  inspectionAdvertisementId,
+  comparisonAdvertisementId,
+}: {
+  inspectionAdvertisementId: string;
+  comparisonAdvertisementId: string;
+}) => {
+  return instance.get(`/api/v1/same-ad/result/${inspectionAdvertisementId}/detail/${comparisonAdvertisementId}`);
+};
