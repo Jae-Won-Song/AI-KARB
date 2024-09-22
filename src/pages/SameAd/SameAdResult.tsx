8945169a121e2432af64b823720ef62fd344a0c3
oxlzlo <yeojin58@naver.com>
2024-09-23 01:41:09 +0900

    Feat: FE-126 유사율 리스트 페이지 데이터 연결

--- src/pages/SameAd/SameAdResult.tsx
+++ src/pages/SameAd/SameAdResult.tsx
@@ -4,15 +4,48 @@ import SameAdResultBox from '../../components/SameAdResultBox';
 import AdInfoTable from '../../components/AdInfoTable';
 import ReviewAdNumber from '../../components/ReviewAdNumber';
 import ReviewAdResult from '../../components/ReviewAdResult';
-import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import fileCheck from '../../assets/icon-file-check.svg';
 import fileSearch from '../../assets/icon-file-search.svg';
 
+type SameAdType = {
+  advertiser: string;
+  category: string;
+  id: string;
+  postDate: string;
+  product: string;
+  sameSentenceCount: number;
+  similarityPercent: number;
+};
+
 const SameAdResult = () => {
   const [isOpenDetail, setIsOpenDetail] = useState(false);
+  const [sameAd, setSameAd] = useState<SameAdType[]>([]);
 
   const navigate = useNavigate();
+  const location = useLocation();
+
+  // 동일광고 목록 페이지에서 요청한 데이터 응답
+  const adDetails = location.state?.adDetails;
+
+  useEffect(() => {
+    console.log(adDetails);
+
+    if (adDetails && adDetails.adSimilarityInfoList) {
+      setSameAd(
+        adDetails.adSimilarityInfoList.map((sameAd: SameAdType, index: number) => ({
+          advertiser: sameAd.advertiser,
+          category: sameAd.category,
+          id: sameAd.id,
+          postDate: sameAd.postDate,
+          product: sameAd.product,
+          sameSentenceCount: sameAd.sameSentenceCount,
+          similarityPercent: sameAd.similarityPercent,
+        })),
+      );
+    }
+  }, [adDetails]);
 
   const goBack = () => {
     navigate(-1);
@@ -26,6 +59,13 @@ const SameAdResult = () => {
     setIsOpenDetail(true);
   };
 
+  const getShortProductName = (product: string) => {
+    if (product.length > 10) {
+      return `${product.substring(0, 10)}...`;
+    }
+    return product;
+  };
+
   return (
     <main className="sameAdResult">
       <article className="sameAdResult__wrapperLeft">
@@ -33,10 +73,20 @@ const SameAdResult = () => {
           <img src={arrowLeft} alt="뒤로가기 화살표" />
         </button>
         <div className="sameAdResult__wrapperLeft_contents">
-          <ReviewAdNumber adTitle="검수 광고" adNumber="A12345" imgSrc={fileSearch} />
-          <AdInfoTable title1="상품명" title2="광고주" content1="명작수" content2="아모레퍼시픽 코리아" />
-          <AdInfoTable title1="업종구분" title2="게재일" content1="식품/음료" content2="2024-07-26" />
-          <div className="sameAdResult__wrapperLeft_contents_article">기사내용 (스크롤 테스트 완료)</div>
+          <ReviewAdNumber adTitle="검수 광고" adNumber={adDetails?.inspectionAdInfo?.id} imgSrc={fileSearch} />
+          <AdInfoTable
+            title1="상품명"
+            title2="광고주"
+            content1={adDetails?.inspectionAdInfo?.product}
+            content2={adDetails?.inspectionAdInfo?.advertiser}
+          />
+          <AdInfoTable
+            title1="업종구분"
+            title2="게재일"
+            content1={adDetails?.inspectionAdInfo?.category}
+            content2={adDetails?.inspectionAdInfo?.postDate}
+          />
+          <div className="sameAdResult__wrapperLeft_contents_article">{adDetails?.inspectionAdInfo?.content}</div>
         </div>
       </article>
       <div className="middleBar"> </div>
@@ -78,11 +128,24 @@ const SameAdResult = () => {
           </div>
         ) : (
           <div className="sameAdResult__wrapperRight_contents">
-            <ReviewAdResult reviewNumber={4} detailSpan="동일 광고는 유사도 80% 이상의 광고만 간주합니다." />
+            <ReviewAdResult
+              reviewNumber={adDetails?.adSimilarityInfoList.length}
+              detailSpan="동일 광고는 유사도 80% 이상의 광고만 간주합니다."
+            />
             <div className="sameAdResult__wrapperRight_contents_resultBox">
-              <SameAdResultBox onClick={openDetail} />
-              <SameAdResultBox onClick={openDetail} />
-              <SameAdResultBox onClick={openDetail} />
+              {sameAd.map((ad, index) => (
+                <SameAdResultBox
+                  key={index}
+                  id={ad.id}
+                  product={getShortProductName(ad.product)}
+                  advertiser={ad.advertiser}
+                  category={ad.category}
+                  postDate={ad.postDate}
+                  similarityPercent={ad.similarityPercent}
+                  sameSentenceCount={ad.sameSentenceCount}
+                  onClick={openDetail}
+                />
+              ))}
             </div>
           </div>
         )}
